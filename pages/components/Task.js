f18f635a36911c598c01bbdf50624c3e2f65b2a8
zbytes2227 <zbytes1@outlook.com>
2023-05-03 14:38:44 +0530

    fixed undefined vars

--- pages/components/Task.js
+++ pages/components/Task.js
@@ -1,20 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Task = (props) => {
+	// let val;
+	// useEffect(() => {
+	// 	if (!props.task){
+	// 		val = true
+	// 	}
+	// }, [])
+	
 	const [dropDown, setdropDown] = useState(true);
-	const [State1, setState1] = useState(props.task.is_completed)
+	const [State1, setState1] = useState(props.task && props.task.is_completed)
 	return (
   <li className="py-2 text-gray-600 my-2 border-b-2 border-gray-100 bg-slate-50 rounded-lg shadow-lg cursor-pointer">
 				<div className='rounded cursor-pointer'>
 					<input className="hidden" type="checkbox" id="task_1" checked={State1} />
 					<label className="flex items-center justify-between h-10 px-2 cursor-pointer" htmlFor="task_1">
-						<div className='flex' onClick={() => { setState1(!State1), props.toggleComplete(props.task._id, props.task.is_completed, props.task.task_name) }}>
+						<div className='flex' onClick={() => { setState1(!State1), props.toggleComplete(props.task._id, State1, props.task.task_name) }}>
 							<span className="flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full">
 								<svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
 									<path tabIndex="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
 								</svg>
 							</span>
-							<span className="ml-4 text-md">{props.task.task_name}</span>
+							<span className="ml-4 text-md">{props.task && props.task.task_name}</span>
 						</div>
 
 						<div></div>
